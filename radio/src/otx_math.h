2c4938036c08264dea21205de4cabe2455eee43e
Chevy Hu <chevyuhf@gmail.com>
2018-09-05 21:03:05 +0800

    Add menu string in en.h.txt, add return back to calcRESXto1000 as it was be.

--- radio/src/otx_math.h
+++ radio/src/otx_math.h
@@ -117,7 +117,7 @@ inline int calc1000toRESX(int x)
 
 inline int calcRESXto1000(int x)
 {
-  divRoundClosest(x*1000, RESX);
+  return divRoundClosest(x*1000, RESX);
 }
 
 inline int calcRESXto100(int x)
